178ae46d4403ff90c2835cdc8ac55d3afe66fc06
Nguyen QuocDung <dungnq.95@gmail.com>
2024-08-13 00:43:09 +0900

    Initial commit

---
+++ components/ImageLink.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <a :href="link" target="_blank">
+        <img :src="imageSrc" :alt="altText" class="image" />
+      </a>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      imageSrc: {
+        type: String,
+        required: true
+      },
+      link: {
+        type: String,
+        required: true
+      },
+      altText: {
+        type: String,
+        default: 'Image'
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .image {
+    width: 100%;
+    max-width: 500px;
+    cursor: pointer;
+  }
+  </style>
+  
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ImageLink 
+      imageSrc="/images/udnuasfhc.jpg" 
+      link="https://www.facebook.com/" 
+      altText="Description of image" 
+    />
+  </div>
+</template>
+
+<script>
+import ImageLink from '~/components/ImageLink.vue'
+
+export default {
+  components: {
+    ImageLink
+  }
+}
+</script>
+
+<style>
+/* Add any additional styles you need here */
+</style>
